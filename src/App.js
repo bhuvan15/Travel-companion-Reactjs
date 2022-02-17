2a21721e1288ca40cec40cec2828bf47be042dfe
bhuvan15 <bhuvanmalik321@gmail.com>
2022-02-17 22:22:45 +0530

    Layout Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header/Header'
+import List from './components/List/List'
+import Map from './components/Map/Map'
+
+/* Materail UI */
+import {CssBaseline, Grid} from '@material-ui/core'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* CSSBaseLine removes basics styles*/}
+      <CssBaseline />
+      {/* Header */}
+      <Header/>
+      {/* Body Grid */}
+      <Grid container spacing = {3} style = {{width: '100%'}} >
+          
+          {/* xs and md are the breakpoints for space taken on small devices */}
+
+          {/* List */}
+          <Grid item xs={12} md = {4}>
+            <List />
+          </Grid>
+          
+          {/* Map */}
+          <Grid item xs={12} md = {8}>
+            <Map />
+          </Grid>
+      </Grid>
     </div>
   );
 }
